203899a2c83250c04e02a03bcd904a31d2130036
Trix <dtwatson443@gmail.com>
2021-01-23 16:28:17 -0600

    Datasets

---
+++ src/ReplicatedStorage/Packages/Metadata/Changelog.lua
@@ -0,0 +1,12 @@
+return {
+
+    --[[ 1/23/21 ]]
+    {
+        Date = { Month = 1, Day = 23, Year = 2021 },
+        Log = {
+            "Hello World!",
+            "Added changelog",
+            "Added new camera framework"
+        }
+    }
+}
